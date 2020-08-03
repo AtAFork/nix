710befef0390d5bddaf138febfc8117eca424163
Adam <AtAFork@users.noreply.github.com>
2020-08-03 11:41:45 -0500

    Update rpi.nix

--- rpi.nix
+++ rpi.nix
@@ -40,8 +40,8 @@
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
 
-  # Use 1GB of additional swap memory in order to not run out of memory
+  # Use 2GB of additional swap memory in order to not run out of memory
   # when installing lots of things while running other things at the same time.
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+  swapDevices = [ { device = "/swapfile"; size = 2048; } ];
 
 }
